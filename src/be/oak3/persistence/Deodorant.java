a40c25a26808f73f91e51233e303337c7bc2b89f
Radek <radoslaw.lizak@gmail.com>
2017-09-13 14:08:48 +0200

    class Deodorant v1 class Parfum v1

---
+++ src/be/oak3/persistence/Deodorant.java
@@ -0,0 +1,28 @@
+package be.oak3.persistence;
+
+import java.util.Comparator;
+
+public class Deodorant extends Producten{
+    private enum DeoType {VAPO, STICK};
+    private DeoType deoType;
+
+    public Deodorant(int productNummer, String merk, String naam, int volume, double prijs, DeoType deoType) {
+        super(productNummer, merk, naam, volume, prijs);
+        this.deoType = deoType;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\t" + soort;
+    }
+
+    @Override
+    public int compare(Producten o1, Producten o2) {
+        return 0;
+    }
+
+    @Override
+    public Comparator<Producten> reversed() {
+        return null;
+    }
+}
---
+++ src/be/oak3/persistence/Parfum.java
@@ -0,0 +1,5 @@
+package be.oak3.persistence;
+
+public class Parfum extends Producten {
+
+}
