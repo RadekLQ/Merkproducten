0b541cc0124ea5969d3a1f3692632e3c644231bd
Radek <radoslaw.lizak@gmail.com>
2017-09-13 14:00:00 +0200

    class Aftershave v1.1 class Deodorant temp

--- src/be/oak3/persistence/Aftershave.java
+++ src/be/oak3/persistence/Aftershave.java
@@ -1,6 +1,8 @@
 package be.oak3.persistence;
 
-public abstract class Aftershave extends Producten {
+import java.util.Comparator;
+
+public class Aftershave extends Producten {
     private enum Soort {VAPO, GEL};
     private Soort soort;
 
@@ -12,6 +14,16 @@ public abstract class Aftershave extends Producten {
     @Override
     public String toString() {
         return super.toString() + "\t" + soort;
+    }
+
+    @Override
+    public int compare(Producten o1, Producten o2) {
+        return 0;
+    }
+
+    @Override
+    public Comparator<Producten> reversed() {
+        return null;
     }
 
 
