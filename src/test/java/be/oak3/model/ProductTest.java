1cc482cc2750a5ef8044a8c4db9887e78c3be61e
Radek <radoslaw.lizak@gmail.com>
2017-09-19 14:08:24 +0200

    v 2.4.2 jUnit integration in progress Product test integrated with jUnit & AssertJ

---
+++ src/test/java/be/oak3/model/ProductTest.java
@@ -0,0 +1,45 @@
+package be.oak3.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+//http://joel-costigliola.github.io/assertj/
+
+public class ProductTest {
+    private Product parfum;
+
+    Product product = new Parfum(1001, "Dolce & Gabbana", "Light Blue", 100, 66.72);
+
+//  jUnit
+
+    @Before
+    public void init() {
+        parfum =  new Parfum(1001, "BVLGARI", "BLV", 75, 61.52);
+    }
+
+    @Test
+    public void testProduct() {
+        Assert.assertEquals("Testing ProductNummer", 1001, parfum.getProductNummer(), 0.000001);
+        Assert.assertEquals("Testing merk", "BVLGARI", parfum.getMerk());
+        Assert.assertEquals("Testing naam", "BLV", parfum.getNaam());
+        Assert.assertEquals("Testing volume", 75, parfum.getVolume(), 0.000001);
+        Assert.assertEquals("Testing prijs", 61.52, parfum.getPrijs(), 0.000001);
+    }
+
+//  AssertJ
+    @Test
+    public void compareProduct() {
+
+        assertThat(product.getProductNummer()).isEqualTo(1001);
+        assertThat(product.getMerk()).isEqualTo("Dolce & Gabbana");
+        assertThat(product.getNaam()).isEqualTo("Light Blue");
+        assertThat(product.getVolume()).isEqualTo(100);
+        assertThat(product.getPrijs()).isEqualTo(66.72);
+        assertThat(product).isNotNull();
+        assertThat(product).isInstanceOf(Parfum.class);
+        assertThat(product).isInstanceOf(Product.class);
+    }
+}
