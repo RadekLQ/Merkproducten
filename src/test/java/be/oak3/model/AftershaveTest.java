eafea10e958598c64ddefa84fc7462c59901ad6c
Radek <radoslaw.lizak@gmail.com>
2017-09-19 14:46:41 +0200

    v 2.4.2 jUnit integration in progress AftershaveTest integrated with AssertJ

--- src/test/java/be/oak3/model/AftershaveTest.java
+++ src/test/java/be/oak3/model/AftershaveTest.java
@@ -6,17 +6,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AftershaveTest {
 
-    Aftershave aftershave = new Aftershave(0, "Yves Saint Laurent", "Jazz", 50, 39.84,
+    Aftershave aftershave = new Aftershave(1002, "Yves Saint Laurent", "Jazz", 50, 39.84,
             Aftershave.Soort.VAPO);
 
     @Test
-    public void testToString()  {
+    public void testToString() {
         assertThat(aftershave).isInstanceOfAny(Product.class);
         assertThat(aftershave).isInstanceOf(Product.class);
         assertThat(aftershave).isInstanceOf(Aftershave.class);
-        assertThat(aftershave).isInstanceOf(Aftershave.Soort.VAPO);
+        assertThat(aftershave).hasToString(Aftershave.Soort.VAPO.toString());
         assertThat(aftershave).isNotNull();
 
+        assertThat(aftershave.getProductNummer()).isEqualTo(1002);
+        assertThat(aftershave.getMerk()).isEqualTo("Yves Saint Laurent");
+        assertThat(aftershave.getNaam()).isEqualTo("Jazz");
+        assertThat(aftershave.getVolume()).isEqualTo(50);
+        assertThat(aftershave.getPrijs()).isEqualTo(39.84);
     }
-
 }
---
+++ src/test/java/be/oak3/model/DeodorantTest.java
@@ -0,0 +1,14 @@
+package be.oak3.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeodorantTest {
+
+
+    @Test
+    public void toString()  {
+    }
+
+}
