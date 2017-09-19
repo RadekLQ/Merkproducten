da7fa2c6b602ea9c6098934ca5fa2b4260133225
Radek <radoslaw.lizak@gmail.com>
2017-09-19 14:54:00 +0200

    v 2.4.2 jUnit integration in progress DeodorantTest integrated with AssertJ

--- src/test/java/be/oak3/model/DeodorantTest.java
+++ src/test/java/be/oak3/model/DeodorantTest.java
@@ -2,13 +2,25 @@ package be.oak3.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeodorantTest {
 
+    Deodorant deodorant = new Deodorant(1003, "DKNY", "Be Delicious Women", 100, 33.65,
+            Deodorant.DeoType.STICK);
 
     @Test
-    public void toString()  {
-    }
+    public void testToString() {
+        assertThat(deodorant).isInstanceOfAny(Product.class);
+        assertThat(deodorant).isInstanceOf(Product.class);
+        assertThat(deodorant).isInstanceOf(Deodorant.class);
+        assertThat(deodorant).hasToString(Deodorant.DeoType.STICK.toString());
+        assertThat(deodorant).isNotNull();
 
+        assertThat(deodorant.getProductNummer()).isEqualTo(1003);
+        assertThat(deodorant.getMerk()).isEqualTo("DKNY");
+        assertThat(deodorant.getNaam()).isEqualTo("Be Delicious Women");
+        assertThat(deodorant.getVolume()).isEqualTo(100);
+        assertThat(deodorant.getPrijs()).isEqualTo(33.65);
+    }
 }
