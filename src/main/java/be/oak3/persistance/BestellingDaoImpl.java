dad4ffc099c03178586535047b2b8821e2fdab4c
Radek <radoslaw.lizak@gmail.com>
2017-09-25 11:10:45 +0200

    v 2.6.1 BestellingDaoImpl.class connected to DB JDBC integration continued

--- src/main/java/be/oak3/persistance/BestellingDaoImpl.java
+++ src/main/java/be/oak3/persistance/BestellingDaoImpl.java
@@ -2,12 +2,46 @@ package be.oak3.persistance;
 
 import be.oak3.model.Product;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 public class BestellingDaoImpl implements Bestelling {
 
+    public static void main(String[] args) {
+        Properties props = new Properties();
+
+        try {
+            FileInputStream fis = new FileInputStream("Merkproducten.properties");
+            props.load(fis);
+            fis.close();
+
+            //load the Driver Class
+            String driver = props.getProperty("DB_DRIVER_CLASS");
+            if (driver != null) {
+                Class.forName(driver);
+            }
+
+            //create the connection now
+            String url = props.getProperty("DB_URL");
+            String username = props.getProperty("DB_USERNAME");
+            String password = props.getProperty("DB_PASSWORD");
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            System.out.println("Connect to DB OK");
+        } catch (IOException | ClassNotFoundException | SQLException ex) {
+            System.out.println("Oops, something went wrong!");
+            ex.printStackTrace(System.err);
+        }
+    }
+
     @Override
     public void voegProductToe(Product product) {
+
     }
 
     @Override
